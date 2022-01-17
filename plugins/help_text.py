c937fd180849a92e708af249ee9c0d25db1c6e88
joker153 <77005994+joker153@users.noreply.github.com>
2022-01-17 16:54:19 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -102,12 +102,12 @@ async def start_me(bot, update):
                         InlineKeyboardButton("Help", callback_data = "ghelp")
                 ],
                 [
-                    InlineKeyboardButton('Support Channel', url='https://t.me/Mai_bOTs'),
-                    InlineKeyboardButton('Feedback', url='https://t.me/No_OnE_Kn0wS_Me')
+                    InlineKeyboardButton('Webseries', url='https://t.me/m2_series'),
+                    InlineKeyboardButton('Feedback', url='https://t.me/+-XZsS6y66zI0YWZl')
                 ],
                 [
-                    InlineKeyboardButton('Other Bots', url='https://t.me/Mai_bOTs/17'),
-                    InlineKeyboardButton('Source', url='https://github.com/No-OnE-Kn0wS-Me/FileRenameBot')
+                    InlineKeyboardButton('Movieupdates', url='https://t.me/movieuploaders_link'),
+                    InlineKeyboardButton('Group', url='https://t.me/+-XZsS6y66zI0YWZl')
                 ]
             ]
         ),
